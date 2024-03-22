8fd624313e1dc4c611247e16f12b29f2ee952e3c
ThaliaBravo <thaliasolerbravo2@gmail.com>
2024-03-22 17:44:39 +0100

    cambios en la logica del formulario

--- JavaScript/validacionFormulario.js
+++ JavaScript/validacionFormulario.js
@@ -1,28 +1,29 @@
-function validarFormulario(){
-    var fecha = document.getElementById("fecha").value;
-    var fechaNacimiento = new Date(fecha);
-    var fechaActual = new Date();
-    var edad = fechaActual.getFullYear() - fechaNacimiento.getFullYear();
-    if(edad < 18){
-        mostrarError("Debes ser mayor de edad para registrarte");
-        return false;
-    }
 
-    var nick = document.getElementById("nickname").value;
-    if(!isNaN(nick.charAt(nick.length-1))){
-        mostrarError("El nick debe acabar en un número");
-        return false;
-    }
+        function validarFormulario() {
+            var fecha = document.getElementById("fecha").value;
+            var fechaNacimiento = new Date(fecha);
+            var fechaActual = new Date();
+            var edad = fechaActual.getFullYear() - fechaNacimiento.getFullYear();
+            if (edad < 18) {
+                mostrarError("Debes ser mayor de edad para registrarte");
+                return false;
+            }
 
-    var email = document.getElementById("correo").value;
-    if(email.indexOf("@itb.cat")==-1){
-        mostrarError("El correo debe ser del ITB");
-        return false;
-    }
-    return true;
-}
+            var nick = document.getElementById("nickname").value;
+            if (!isNaN(nick.charAt(nick.length - 1))) {
+                mostrarError("El nick debe acabar en un número");
+                return false;
+            }
 
-function mostrarError(mensaje){
-    var mensajeError=document.getElementById("mensajeError");
-    mensajeError.innerHTML=mensaje;
-}
+            var email = document.getElementById("correo").value;
+            if (email.indexOf("@itb.cat") == -1) {
+                mostrarError("El correo debe ser del ITB");
+                return false;
+            }
+            return true;
+        }
+
+        function mostrarError(mensaje) {
+            var mensajeError = document.getElementById("mensajeError");
+            mensajeError.innerHTML = mensaje;
+        }
